859374cc250a7dc718163801ec26df800849fd51
leewoojin <zxsa999o@gmail.com>
2025-03-06 12:57:35 +0900

    [feat] 스웨거

--- package404/src/main/java/com/example/package404/instructor/controller/CourseController.java
+++ package404/src/main/java/com/example/package404/instructor/controller/CourseController.java
@@ -9,6 +9,7 @@ import com.example.package404.instructor.model.dto.res.InstructorCourseListRespo
 import com.example.package404.instructor.model.dto.res.CourseResponseDto;
 import com.example.package404.instructor.service.CourseService;
 import com.example.package404.user.model.User;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -26,47 +27,37 @@ public class CourseController {
 
 
 
-    //Todo baseResponse
-    // o 근데 기수만 쫌 자동생성되게
+    //Todo n+1
+    @Operation(summary = "코스 정보 등록", description = "강사가 새로운 코스를 등록하는 기능입니다.")
     @PostMapping("/register")
     public BaseResponse register(@AuthenticationPrincipal User user, @RequestBody CourseRegister dto) {
-
-
         courseService.register(dto, user);
-
         return baseResponseService.getSuccessResponse(InstructorResponseStatus.SUCCESS);
-
     }
 
 
 
-    //Todo baseResponse
-    // 강사가 맡았던 기수 조회 o
+    //Todo n+1
+    @Operation(summary = "강사가 맡았던 기수 조회", description = "특정 강사가 맡았던 기수들의 코스를 조회하는 기능입니다.")
     @GetMapping("/instructor/{userIdx}")
     public BaseResponse<List<InstructorCourseListResponseDto>> getInstructorCourse(@PathVariable Long userIdx) {
         List<InstructorCourseListResponseDto> response = courseService.findIstructorCourse(userIdx);
-
         return baseResponseService.getSuccessResponse(response, InstructorResponseStatus.SUCCESS);
     }
 
-    //Todo baseResponse
-    // O
-    //교과목 별 조회
+
+    //Todo n+1
+    @Operation(summary = "교과목 별 커리큘럼 조회", description = "특정 교과목에 대한 커리큘럼 정보를 조회하는 기능입니다.")
     @GetMapping("/curriculum")
-    public  BaseResponse<List<CurriculumResponseDto>>getCurriculumBySubject(@RequestParam String subject) {
-
-
+    public BaseResponse<List<CurriculumResponseDto>> getCurriculumBySubject(@RequestParam String subject) {
         List<CurriculumResponseDto> response = courseService.getCurriculumBySubject(subject);
-
         return baseResponseService.getSuccessResponse(response, InstructorResponseStatus.SUCCESS);
     }
 
 
 
-
-    //Todo baseResponse
-    // o
-    // 이건 현재 기수들 코스 조회하는거
+    //Todo n+1
+    @Operation(summary = "기수 별 코스 조회", description = "특정 기수에 대한 코스 정보를 조회하는 기능입니다.")
     @GetMapping("/{generation}")
     public BaseResponse<CourseResponseDto> read(@PathVariable int generation) {
         CourseResponseDto response = courseService.read(generation);
