b64a208c8acad4252d092c8f96cf6c53f27b7b45
goodleaf96 <ky1234tho@gmail.com>
2025-03-23 23:20:36 +0900

    중복 url 제거

--- package404/src/main/java/com/example/package404/user/controller/UserController.java
+++ package404/src/main/java/com/example/package404/user/controller/UserController.java
@@ -66,7 +66,7 @@ public class UserController {
     }
 
     @Operation(summary = "회원정보 조회", description = "로그인한 사용자의 정보를 조회합니다.")
-    @GetMapping("/info")
+    @GetMapping("/gettest")
     public String getTestVersion() {
 
         return "test1";
